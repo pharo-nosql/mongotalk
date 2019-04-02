782d4f063be71ea6ef9fe4e1a8fd1a6d76991d39
Norbert Hartl <norbert@hartl.name>
2019-04-02 17:59:40 +0200

    added second deploy key for mongo

--- .install_mongodb_on_travis.sh
+++ .install_mongodb_on_travis.sh
@@ -2,6 +2,7 @@
 
 sudo apt-get remove mongodb-org-server mongodb-org-shell
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 
 if [ "$MONGODB" = "2.6" ]; then
     echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | sudo tee /etc/apt/sources.list.d/mongodb.list
