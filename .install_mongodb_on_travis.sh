6bf7646b144be10945a5a8093901a35ad655c984
Norbert Hartl <norbert@hartl.name>
2019-04-02 18:04:17 +0200

    yet another key added

--- .install_mongodb_on_travis.sh
+++ .install_mongodb_on_travis.sh
@@ -2,6 +2,9 @@
 
 sudo apt-get remove mongodb-org-server mongodb-org-shell
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+# 3.x key???
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+# version 4 key
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 
 if [ "$MONGODB" = "2.6" ]; then
