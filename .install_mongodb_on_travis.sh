176773c496c172ccbf741e39d4eb00f0e0ec8300
Martín Dias <tinchodias@gmail.com>
2020-10-24 19:50:13 -0300

    Add mongodb 4.4, remove 3.6. Also remove P7.

--- .install_mongodb_on_travis.sh
+++ .install_mongodb_on_travis.sh
@@ -7,16 +7,14 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF50
 # version 4 key
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 
-if [ "$MONGODB" = "3.6" ]; then
-    echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
-    sudo apt-get update
-    sudo apt-get install mongodb-org-server=3.6.12 mongodb-org-shell=3.6.12
-    # service should be started automatically
-elif [ "$MONGODB" = "4.0" ]; then
+if [ "$MONGODB" = "4.0" ]; then
     echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
     sudo apt-get update
     sudo apt-get install mongodb-org-server=4.0.9 mongodb-org-shell=4.0.9
-    # service should be started automatically
+elif [ "$MONGODB" = "4.4" ]; then
+    echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
+    sudo apt-get update
+    sudo apt-get install mongodb-org-server=4.4.1 mongodb-org-shell=4.4.1
 else
     echo "Invalid MongoDB version"
     exit 1
